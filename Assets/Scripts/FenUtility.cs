d8cc06078654accc7148e2f43ee17cd597de23cd
shayshunk <60236335+shayshunk@users.noreply.github.com>
2023-08-24 14:21:43 -0400

    Promotion Window
    -Editor didn't save promotion window.
    -It has been remade.
    -Works as expected.

--- Assets/Scripts/FenUtility.cs
+++ Assets/Scripts/FenUtility.cs
@@ -14,6 +14,7 @@ public static class FenUtility
     public const string position4 = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
     public const string position5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ";
     public const string position6 = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ";
+    public const string position7 = "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1";
 
     public static LoadedPositionInfo PositionFromFen(string fen)
     {
--- Assets/Scripts/Managers/BoardManager.cs
+++ Assets/Scripts/Managers/BoardManager.cs
@@ -125,7 +125,7 @@ public class BoardManager : MonoBehaviour
         skipFirstButton.interactable = false;
         skipLastButton.interactable = false;
 
-        LoadPosition(FenUtility.position5);
+        LoadPosition(FenUtility.position7);
     }
 
     public void LoadPosition(string fen)
@@ -230,7 +230,7 @@ public class BoardManager : MonoBehaviour
         plyCount = 0;
         fiftyMoveCounter = 0;
         promotionIndex = 100;
-        depthTest = 3;
+        depthTest = 2;
     }
 
     private void Promotion()
